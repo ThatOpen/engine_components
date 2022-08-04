e0aabf2fa2e32c93ee82b3894c83a2b8c6cdd705
Harry Collin <harry.archie.collin@gmail.com>
2022-08-04 23:38:28 +0100

    Improve colour function and material caching

--- library/fragment/fragment-culling.ts
+++ library/fragment/fragment-culling.ts
@@ -8,6 +8,7 @@ export default class FragmentCulling {
   readonly renderTarget: THREE.WebGLRenderTarget;
   readonly bufferSize: number;
   readonly buffer: Uint8Array
+  readonly materialCache: Map<string, THREE.MeshBasicMaterial>;
 
   constructor(
     private components: Components,
@@ -15,53 +16,69 @@ export default class FragmentCulling {
     readonly updateInterval = 1000,
     readonly rtWidth = 512,
     readonly rtHeight = 512)
+
   {
     this.renderTarget = new THREE.WebGLRenderTarget(this.rtWidth, this.rtHeight);
     this.bufferSize = this.rtWidth * this.rtHeight * 4;
     this.buffer = new Uint8Array(this.bufferSize);
+    this.materialCache = new Map<string, THREE.MeshBasicMaterial>();
     window.setInterval(this.updateVisibility, updateInterval)
   }
 
-  updateVisibility = () => {
+  private getMaterial(r: number, g: number, b: number) {
+    const code = `rgb(${r}, ${g}, ${b})`
+    let material = this.materialCache.get(code);
+    if(!material){
+      material = new THREE.MeshBasicMaterial({ color: new THREE.Color(code) })
+      this.materialCache.set(code, material)
+    }
+    return material;
+  }
+
+
+  private updateVisibility = () => {
 
     this.components.renderer.renderer.setRenderTarget(this.renderTarget)
 
     let r = 0;
     let g = 0;
     let b = 0;
-    let i = 1;
+    let i = 0;
 
     const getNextColor = () => {
-
-      if (i === 1) {
-        r++
-        if (r === 255) {
-          i++
-        }
-      }
-      if (i === 2) {
-        g++
-        if (g === 255) {
-          i++
-        }
-      }
-      if (i === 3) {
+      if(i === 0) {
         b++;
-        if (b === 255) {
-          i++
+        if(b === 256) {
+          b = 0;
+          i = 1
         }
       }
-      if (i < 3) i++;
-      else i = 1;
+      if(i === 1) {
+        g++;
+        i = 0;
+        if(g === 256){
+          g = 0;
+          i = 2
+        }
+      }
+      if(i === 2) {
+        r++;
+        i = 1;
+        if(r === 256){
+          r = 0;
+          i = 0
+        }
+      }
       return {
-        color: new THREE.Color(`rgb(${r}, ${g}, ${b})`),
+        r,
+        g,
+        b,
         code: `${r}${g}${b}`
       }
     }
 
-    const fragmentColorMap = new Map<string, Fragment>();
 
-    const meshesToHideForRender: THREE.Mesh[] = []
+    const fragmentColorMap = new Map<string, Fragment>();
 
     for (const fragment of this.fragment.fragments) {
       // Store original materials
@@ -69,17 +86,14 @@ export default class FragmentCulling {
         fragment.mesh.userData.prevMat = fragment.mesh.material;
       }
 
-      const {color, code} = getNextColor();
-      const mat = new THREE.MeshBasicMaterial({color})
+      const { r, g, b, code} = getNextColor();
+      const mat = this.getMaterial(r, g, b);
       fragmentColorMap.set(code, fragment);
-
-      let shouldBeHidden = false;
 
       if (Array.isArray(fragment.mesh.material)) {
         const matArray: any[] = [];
         for (const material of fragment.mesh.material) {
           if(material.opacity !== 1) {
-            shouldBeHidden = true;
           }
           matArray.push(mat)
         }
@@ -90,26 +104,14 @@ export default class FragmentCulling {
 
         // @ts-ignore
         if(fragment.mesh.material.opacity !== 1) {
-          shouldBeHidden = true;
         }
       }
 
       // Set to visible
       fragment.mesh.visible = true;
-
-      if(shouldBeHidden){
-        meshesToHideForRender.push(fragment.mesh)
-      }
     }
 
-    console.log(meshesToHideForRender)
-
-    //for(const mesh of meshesToHideForRender) mesh.visible = false;
-
     this.components.renderer.renderer.render(this.components.scene.getScene(), this.components.camera.getCamera())
-
-    //for(const mesh of meshesToHideForRender) mesh.visible = true;
-
     this.components.renderer.renderer.readRenderTargetPixels(this.renderTarget, 0, 0, this.rtWidth, this.rtHeight, this.buffer);
 
     for (let i = 0; i < this.bufferSize; i += 4) {
@@ -127,9 +129,9 @@ export default class FragmentCulling {
       fragment.mesh.visible = false;
     }
 
-    for(const fragment of this.fragment.fragments){
-      // Restore material
-      fragment.mesh.material = fragment.mesh.userData.prevMat;
-    }
+    // for(const fragment of this.fragment.fragments){
+    //   // Restore material
+    //   fragment.mesh.material = fragment.mesh.userData.prevMat;
+    // }
   }
 }
