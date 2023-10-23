54f63c4a0daee442110533c5f364cf3787219c37
Óscar Carrasco <oxcabe@gmail.com>
2023-10-23 15:36:51 +0100

    feat(docs): implement typedoc config (#133)
    * feat(docs): implement typedoc config
    
    * fix(package): update index.js path

--- resources/rollup.config.mjs
+++ resources/rollup.config.mjs
@@ -4,7 +4,7 @@ import commonjs from "@rollup/plugin-commonjs";
 
 // This creates the bundle used by the examples
 export default {
-	input: "dist/index.js",
+	input: "dist/src/index.js",
 	output: {
 		file: "./resources/openbim-components.js",
 		format: "esm",
