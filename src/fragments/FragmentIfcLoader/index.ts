b15b92de982c3f49901d1f97f3ecf732f5f089f4
Raphael Stärk <dj.rst@icloud.com>
2024-01-18 13:26:17 +0100

    feat: Set log level of FragmentIfcLoader

--- src/fragments/FragmentIfcLoader/index.ts
+++ src/fragments/FragmentIfcLoader/index.ts
@@ -18,6 +18,7 @@ export * from "./src/types";
 
 export interface FragmentIfcLoaderConfig {
   autoSetWasm: boolean;
+  logLevel?: WEBIFC.LogLevel;
 }
 
 /**
@@ -214,6 +215,9 @@ export class FragmentIfcLoader
     const { path, absolute } = this.settings.wasm;
     this._webIfc.SetWasmPath(path, absolute);
     await this._webIfc.Init();
+    if (this.config.logLevel) {
+      this._webIfc.SetLogLevel(this.config.logLevel);
+    }
     return this._webIfc.OpenModel(data, this.settings.webIfc);
   }
 
