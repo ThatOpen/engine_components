a70747c6e1a28f4b87ac2378e63cebce70332d29
Antonio González Viegas <antoniogviegas@hotmail.com>
2024-06-20 10:52:07 +0200

    chore: build examples

--- examples/assets/exploder.js
+++ examples/assets/exploder.js
@@ -1,15 +1,15 @@
-import{a4 as c}from"./web-ifc-api-CwSt8Jc1.js";import{S as l}from"./stats.min-BpIepu9J.js";import{p as d,J as a,m as i}from"./index-K5IA6oiZ.js";import{p,C as m,s as b,i as u,H as g,d as f,u as h,g as w,A as y}from"./index-DGU7st_s.js";const A=document.getElementById("container"),t=new p,x=t.get(m),e=x.create();e.scene=new b(t);e.renderer=new u(t,A);e.camera=new g(t);t.init();e.camera.controls.setLookAt(12,6,8,0,0,-10);e.scene.setup();const I=t.get(f);I.create(e);e.scene.three.background=null;const L=new h(t),v=await fetch("https://thatopen.github.io/engine_components/resources/small.frag"),C=await v.arrayBuffer(),E=new Uint8Array(C),s=L.load(E);e.scene.three.add(s);const k=await fetch("https://thatopen.github.io/engine_components/resources/small.json");s.setLocalProperties(await k.json());const T=t.get(w),U=t.get(y);U.byIfcRel(s,c,"storeys");const n=new l;n.showPanel(2);document.body.append(n.dom);n.dom.style.left="0px";n.dom.style.zIndex="unset";e.renderer.onBeforeUpdate.add(()=>n.begin());e.renderer.onAfterUpdate.add(()=>n.end());d.init();const o=a.create(()=>i`
+import"./web-ifc-api-CwSt8Jc1.js";import{S as l}from"./stats.min-BpIepu9J.js";import{p,J as a,m as i}from"./index-K5IA6oiZ.js";import{p as d,C as m,s as u,i as b,H as g,d as h,u as f,k as w,g as x,A as y}from"./index-BVLgAR8-.js";const k=document.getElementById("container"),e=new d,v=e.get(m),t=v.create();t.scene=new u(e);t.renderer=new b(e,k);t.camera=new g(e);e.init();t.camera.controls.setLookAt(12,6,8,0,0,-10);t.scene.setup();const A=e.get(h);A.create(t);t.scene.three.background=null;const L=new f(e),S=await fetch("https://thatopen.github.io/engine_components/resources/small.frag"),j=await S.arrayBuffer(),B=new Uint8Array(j),s=L.load(B);t.scene.three.add(s);const C=await fetch("https://thatopen.github.io/engine_components/resources/small.json");s.setLocalProperties(await C.json());const r=e.get(w),E=await fetch("https://thatopen.github.io/engine_components/resources/small-relations.json"),J=r.getRelationsMapFromJSON(await E.text());r.setRelationMap(s,J);const U=e.get(x),_=e.get(y);await _.bySpatialStructure(s);const n=new l;n.showPanel(2);document.body.append(n.dom);n.dom.style.left="0px";n.dom.style.zIndex="unset";t.renderer.onBeforeUpdate.add(()=>n.begin());t.renderer.onAfterUpdate.add(()=>n.end());p.init();const o=a.create(()=>i`
     <bim-panel active label="Exploder Tutorial" class="options-menu">
       <bim-panel-section collapsed label="Controls">
         <bim-checkbox 
           label="Explode model" 
-          @change="${({target:r})=>{T.set(r.value)}}">  
+          @change="${({target:c})=>{U.set(c.value)}}">  
         </bim-checkbox>  
 
       </bim-panel-section>
     </bim-panel>
-    `);document.body.append(o);const R=a.create(()=>i`
+    `);document.body.append(o);const $=a.create(()=>i`
       <bim-button class="phone-menu-toggler" icon="solar:settings-bold"
         @click="${()=>{o.classList.contains("options-menu-visible")?o.classList.remove("options-menu-visible"):o.classList.add("options-menu-visible")}}">
       </bim-button>
-    `);document.body.append(R);
+    `);document.body.append($);
