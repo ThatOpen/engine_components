989be31b43bc7b665c06a81c502a9fcf1ff20c05
Antonio González Viegas <antoniogviegas@hotmail.com>
2024-07-04 13:36:44 +0200

    chore: build examples

--- packages/core/src/core/Components/index.ts
+++ packages/core/src/core/Components/index.ts
@@ -14,7 +14,7 @@ export class Components implements Disposable {
   /**
    * The version of the @thatopen/components library.
    */
-  static readonly release = "2.0.21";
+  static readonly release = "2.0.22";
 
   /** {@link Disposable.onDisposed} */
   readonly onDisposed = new Event<void>();
