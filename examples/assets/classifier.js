32efe3c74569317ead569a5fa642991de15c166f
Juan Hoyos <juan.hoyos4@gmail.com>
2024-07-17 15:38:04 -0500

    chore: builds examples

--- examples/assets/classifier.js
+++ examples/assets/classifier.js
@@ -1,4 +1,4 @@
-import{a2 as d,C as m}from"./web-ifc-api-5J0YW9AE.js";import{S as u}from"./stats.min-BpIepu9J.js";import{m as b,t as a,a as c}from"./index-tywNknxv.js";import{p,C,s as f,i as w,H as I,d as g,h as A,A as y}from"./index-Di1uyhVc.js";const E=document.getElementById("container"),s=new p,L=s.get(C),t=L.create();t.scene=new f(s);t.renderer=new w(s,E);t.camera=new I(s);s.init();t.camera.controls.setLookAt(12,6,8,0,0,-10);t.scene.setup();const h=s.get(g);h.create(t);t.scene.three.background=null;const F=new A(s),R=await fetch("https://thatopen.github.io/engine_components/resources/small.frag"),S=await R.arrayBuffer(),T=new Uint8Array(S),i=F.load(T);t.scene.three.add(i);const o=s.get(y);o.byEntity(i);o.byIfcRel(i,d,"storeys");o.byModel(i.uuid,i);const N=o.find({entities:["IFCWALLSTANDARDCASE"]}),$=o.find({entities:["IFCSLAB"]}),U=o.find({entities:["IFCMEMBER","IFCPLATE"]}),v=o.find({entities:["IFCFURNISHINGELEMENT"]}),B=o.find({entities:["IFCDOOR"]}),D=o.find({models:[i.uuid]}),l=new u;l.showPanel(2);document.body.append(l.dom);l.dom.style.left="0px";l.dom.style.zIndex="unset";t.renderer.onBeforeUpdate.add(()=>l.begin());t.renderer.onAfterUpdate.add(()=>l.end());b.init();const e=new m,r=a.create(()=>c`
+import{a2 as d,C as m}from"./web-ifc-api-5J0YW9AE.js";import{S as u}from"./stats.min-BpIepu9J.js";import{m as b,t as a,a as c}from"./index-tywNknxv.js";import{p,C,s as f,i as w,k as I,d as g,h as A,A as y}from"./index-CAlyvYyy.js";const E=document.getElementById("container"),s=new p,L=s.get(C),t=L.create();t.scene=new f(s);t.renderer=new w(s,E);t.camera=new I(s);s.init();t.camera.controls.setLookAt(12,6,8,0,0,-10);t.scene.setup();const h=s.get(g);h.create(t);t.scene.three.background=null;const F=new A(s),R=await fetch("https://thatopen.github.io/engine_components/resources/small.frag"),S=await R.arrayBuffer(),T=new Uint8Array(S),i=F.load(T);t.scene.three.add(i);const o=s.get(y);o.byEntity(i);o.byIfcRel(i,d,"storeys");o.byModel(i.uuid,i);const N=o.find({entities:["IFCWALLSTANDARDCASE"]}),$=o.find({entities:["IFCSLAB"]}),k=o.find({entities:["IFCMEMBER","IFCPLATE"]}),U=o.find({entities:["IFCFURNISHINGELEMENT"]}),v=o.find({entities:["IFCDOOR"]}),B=o.find({models:[i.uuid]}),l=new u;l.showPanel(2);document.body.append(l.dom);l.dom.style.left="0px";l.dom.style.zIndex="unset";t.renderer.onBeforeUpdate.add(()=>l.begin());t.renderer.onAfterUpdate.add(()=>l.end());b.init();const e=new m,r=a.create(()=>c`
     <bim-panel active label="Classifier Tutorial" class="options-menu">
       <bim-panel-section collapsed label="Controls">
       
@@ -14,28 +14,28 @@ import{a2 as d,C as m}from"./web-ifc-api-5J0YW9AE.js";import{S as u}from"./stats
       
         <bim-color-input 
           label="Curtain walls Color" color="#202932" 
-          @input="${({target:n})=>{e.set(n.color),o.setColor(U,e)}}">
+          @input="${({target:n})=>{e.set(n.color),o.setColor(k,e)}}">
         </bim-color-input>
       
         <bim-color-input 
           label="Furniture Color" color="#202932" 
-          @input="${({target:n})=>{e.set(n.color),o.setColor(v,e)}}">
+          @input="${({target:n})=>{e.set(n.color),o.setColor(U,e)}}">
         </bim-color-input>
       
         <bim-color-input 
           label="Doors Color" color="#202932" 
-          @input="${({target:n})=>{e.set(n.color),o.setColor(B,e)}}">
+          @input="${({target:n})=>{e.set(n.color),o.setColor(v,e)}}">
         </bim-color-input>
                   
         <bim-button 
           label="Reset walls color" 
-          @click="${()=>{o.resetColor(D)}}">  
+          @click="${()=>{o.resetColor(B)}}">  
         </bim-button>  
 
       </bim-panel-section>
     </bim-panel>
-    `);document.body.append(r);const k=a.create(()=>c`
+    `);document.body.append(r);const D=a.create(()=>c`
       <bim-button class="phone-menu-toggler" icon="solar:settings-bold"
         @click="${()=>{r.classList.contains("options-menu-visible")?r.classList.remove("options-menu-visible"):r.classList.add("options-menu-visible")}}">
       </bim-button>
-    `);document.body.append(k);
+    `);document.body.append(D);
--- examples/assets/exploder.js
+++ examples/assets/exploder.js
@@ -1,15 +1,15 @@
-import"./web-ifc-api-5J0YW9AE.js";import{S as l}from"./stats.min-BpIepu9J.js";import{m as p,t as a,a as i}from"./index-tywNknxv.js";import{p as d,C as m,s as b,i as u,H as g,d as h,h as f,k as w,g as x,A as y}from"./index-Di1uyhVc.js";const k=document.getElementById("container"),e=new d,v=e.get(m),t=v.create();t.scene=new b(e);t.renderer=new u(e,k);t.camera=new g(e);e.init();t.camera.controls.setLookAt(12,6,8,0,0,-10);t.scene.setup();const A=e.get(h);A.create(t);t.scene.three.background=null;const L=new f(e),S=await fetch("https://thatopen.github.io/engine_components/resources/small.frag"),j=await S.arrayBuffer(),B=new Uint8Array(j),o=L.load(B);t.scene.three.add(o);const C=await fetch("https://thatopen.github.io/engine_components/resources/small.json");o.setLocalProperties(await C.json());const r=e.get(w),E=await fetch("https://thatopen.github.io/engine_components/resources/small-relations.json"),U=r.getRelationsMapFromJSON(await E.text());r.setRelationMap(o,U);const _=e.get(x),F=e.get(y);await F.bySpatialStructure(o);const n=new l;n.showPanel(2);document.body.append(n.dom);n.dom.style.left="0px";n.dom.style.zIndex="unset";t.renderer.onBeforeUpdate.add(()=>n.begin());t.renderer.onAfterUpdate.add(()=>n.end());p.init();const s=a.create(()=>i`
+import"./web-ifc-api-5J0YW9AE.js";import{S as l}from"./stats.min-BpIepu9J.js";import{m as p,t as a,a as i}from"./index-tywNknxv.js";import{p as d,C as m,s as b,i as u,k as g,d as h,h as f,a as w,O as x,A as y}from"./index-CAlyvYyy.js";const k=document.getElementById("container"),e=new d,v=e.get(m),t=v.create();t.scene=new b(e);t.renderer=new u(e,k);t.camera=new g(e);e.init();t.camera.controls.setLookAt(12,6,8,0,0,-10);t.scene.setup();const A=e.get(h);A.create(t);t.scene.three.background=null;const L=new f(e),S=await fetch("https://thatopen.github.io/engine_components/resources/small.frag"),j=await S.arrayBuffer(),B=new Uint8Array(j),o=L.load(B);t.scene.three.add(o);const C=await fetch("https://thatopen.github.io/engine_components/resources/small.json");o.setLocalProperties(await C.json());const r=e.get(w),E=await fetch("https://thatopen.github.io/engine_components/resources/small-relations.json"),O=r.getRelationsMapFromJSON(await E.text());r.setRelationMap(o,O);const U=e.get(x),_=e.get(y);await _.bySpatialStructure(o);const n=new l;n.showPanel(2);document.body.append(n.dom);n.dom.style.left="0px";n.dom.style.zIndex="unset";t.renderer.onBeforeUpdate.add(()=>n.begin());t.renderer.onAfterUpdate.add(()=>n.end());p.init();const s=a.create(()=>i`
     <bim-panel active label="Exploder Tutorial" class="options-menu">
       <bim-panel-section collapsed label="Controls">
         <bim-checkbox 
           label="Explode model" 
-          @change="${({target:c})=>{_.set(c.value)}}">  
+          @change="${({target:c})=>{U.set(c.value)}}">  
         </bim-checkbox>  
 
       </bim-panel-section>
     </bim-panel>
-    `);document.body.append(s);const H=a.create(()=>i`
+    `);document.body.append(s);const F=a.create(()=>i`
       <bim-button class="phone-menu-toggler" icon="solar:settings-bold"
         @click="${()=>{s.classList.contains("options-menu-visible")?s.classList.remove("options-menu-visible"):s.classList.add("options-menu-visible")}}">
       </bim-button>
-    `);document.body.append(H);
+    `);document.body.append(F);
