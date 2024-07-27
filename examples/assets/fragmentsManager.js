6cc4be6c6382bb5a430171f7e7fa53766925cb54
Juan Hoyos <juan.hoyos4@gmail.com>
2024-07-26 19:32:33 -0500

    chore: builds examples

--- examples/assets/fragmentsManager.js
+++ examples/assets/fragmentsManager.js
@@ -1,4 +1,4 @@
-import"./web-ifc-api-CfQNUy7g.js";import{S as u}from"./stats.min-BpIepu9J.js";import{p,C as b,i as g,n as f,W as w,d as h,h as y}from"./index-CrrO1BJs.js";import{m as k,t as l,a as d}from"./index-tywNknxv.js";const L=document.getElementById("container"),n=new p,v=n.get(b),e=v.create();e.scene=new g(n);e.renderer=new f(n,L);e.camera=new w(n);n.init();e.camera.controls.setLookAt(12,6,8,0,0,-10);e.scene.setup();const x=n.get(h);x.create(e);e.scene.three.background=null;const s=n.get(y);let m="";async function F(){if(s.groups.size)return;const t=await(await fetch("https://thatopen.github.io/engine_components/resources/small.frag")).arrayBuffer(),c=new Uint8Array(t),i=s.load(c);e.scene.three.add(i),m=i.uuid}function U(o){const t=document.createElement("a");t.href=URL.createObjectURL(o),t.download=o.name,document.body.appendChild(t),t.click(),t.remove()}function B(){if(!s.groups.size)return;const o=s.groups.get(m);if(!o)return;const t=s.export(o),c=new Blob([t]),i=new File([c],"small.frag");U(i)}function C(){s.dispose()}const a=new u;a.showPanel(2);document.body.append(a.dom);a.dom.style.left="0px";a.dom.style.zIndex="unset";e.renderer.onBeforeUpdate.add(()=>a.begin());e.renderer.onAfterUpdate.add(()=>a.end());k.init();const r=l.create(()=>d`
+import"./web-ifc-api-Du2rJhcJ.js";import{S as u}from"./stats.min-BpIepu9J.js";import{p,C as b,o as g,r as f,W as w,d as h,h as y}from"./index-BPLzurvS.js";import{m as k,t as l,a as d}from"./index-tywNknxv.js";const L=document.getElementById("container"),n=new p,v=n.get(b),e=v.create();e.scene=new g(n);e.renderer=new f(n,L);e.camera=new w(n);n.init();e.camera.controls.setLookAt(12,6,8,0,0,-10);e.scene.setup();const x=n.get(h);x.create(e);e.scene.three.background=null;const s=n.get(y);let m="";async function F(){if(s.groups.size)return;const t=await(await fetch("https://thatopen.github.io/engine_components/resources/small.frag")).arrayBuffer(),c=new Uint8Array(t),r=s.load(c);e.scene.three.add(r),m=r.uuid}function U(o){const t=document.createElement("a");t.href=URL.createObjectURL(o),t.download=o.name,document.body.appendChild(t),t.click(),t.remove()}function B(){if(!s.groups.size)return;const o=s.groups.get(m);if(!o)return;const t=s.export(o),c=new Blob([t]),r=new File([c],"small.frag");U(r)}function C(){s.dispose()}const a=new u;a.showPanel(2);document.body.append(a.dom);a.dom.style.left="0px";a.dom.style.zIndex="unset";e.renderer.onBeforeUpdate.add(()=>a.begin());e.renderer.onAfterUpdate.add(()=>a.end());k.init();const i=l.create(()=>d`
     <bim-panel active label="Fragments Manager Tutorial" class="options-menu">
       <bim-panel-section collapsed label="Controls">
       
@@ -19,8 +19,8 @@ import"./web-ifc-api-CfQNUy7g.js";import{S as u}from"./stats.min-BpIepu9J.js";im
         
       </bim-panel-section>
     </bim-panel>
-    `);document.body.append(r);const $=l.create(()=>d`
+    `);document.body.append(i);const $=l.create(()=>d`
       <bim-button class="phone-menu-toggler" icon="solar:settings-bold"
-        @click="${()=>{r.classList.contains("options-menu-visible")?r.classList.remove("options-menu-visible"):r.classList.add("options-menu-visible")}}">
+        @click="${()=>{i.classList.contains("options-menu-visible")?i.classList.remove("options-menu-visible"):i.classList.add("options-menu-visible")}}">
       </bim-button>
     `);document.body.append($);
--- examples/assets/lengthMeasurement.js
+++ examples/assets/lengthMeasurement.js
@@ -1,4 +1,4 @@
-import{B as m,M as b,a as d}from"./web-ifc-api-CfQNUy7g.js";import{S as p}from"./stats.min-BpIepu9J.js";import{p as u,C as h,i as w,W as k,d as g}from"./index-CrrO1BJs.js";import{m as f,t as l,a as c}from"./index-tywNknxv.js";import{S as D,_ as y}from"./index-BREiuDZQ.js";const r=document.getElementById("container"),t=new u,v=t.get(h),e=v.create();e.scene=new w(t);e.renderer=new D(t,r);e.camera=new k(t);t.init();e.camera.controls.setLookAt(5,5,5,0,0,0);e.scene.setup();const x=t.get(g);x.create(e);e.scene.three.background=null;const M=new m(3,3,3),B=new b({color:"#6528D7"}),i=new d(M,B);i.position.set(0,1.5,0);e.scene.three.add(i);e.meshes.add(i);const n=t.get(y);n.world=e;n.enabled=!0;n.snapDistance=1;r.ondblclick=()=>n.create();window.onkeydown=o=>{(o.code==="Delete"||o.code==="Backspace")&&n.delete()};const s=new p;s.showPanel(2);document.body.append(s.dom);s.dom.style.left="0px";s.dom.style.zIndex="unset";e.renderer.onBeforeUpdate.add(()=>s.begin());e.renderer.onAfterUpdate.add(()=>s.end());f.init();const a=l.create(()=>c`
+import{B as m,M as b,a as d}from"./web-ifc-api-Du2rJhcJ.js";import{S as p}from"./stats.min-BpIepu9J.js";import{p as u,C as h,o as w,W as k,d as g}from"./index-BPLzurvS.js";import{m as f,t as i,a as c}from"./index-tywNknxv.js";import{S as D,_ as y}from"./index-BUF35rMs.js";const r=document.getElementById("container"),t=new u,v=t.get(h),e=v.create();e.scene=new w(t);e.renderer=new D(t,r);e.camera=new k(t);t.init();e.camera.controls.setLookAt(5,5,5,0,0,0);e.scene.setup();const x=t.get(g);x.create(e);e.scene.three.background=null;const M=new m(3,3,3),B=new b({color:"#6528D7"}),l=new d(M,B);l.position.set(0,1.5,0);e.scene.three.add(l);e.meshes.add(l);const n=t.get(y);n.world=e;n.enabled=!0;n.snapDistance=1;r.ondblclick=()=>n.create();window.onkeydown=o=>{(o.code==="Delete"||o.code==="Backspace")&&n.delete()};const s=new p;s.showPanel(2);document.body.append(s.dom);s.dom.style.left="0px";s.dom.style.zIndex="unset";e.renderer.onBeforeUpdate.add(()=>s.begin());e.renderer.onAfterUpdate.add(()=>s.end());f.init();const a=i.create(()=>c`
   <bim-panel active label="Length Measurement Tutorial" class="options-menu">
       <bim-panel-section collapsed label="Controls">
           <bim-label>Create dimension: Double click</bim-label>  
@@ -24,7 +24,7 @@ import{B as m,M as b,a as d}from"./web-ifc-api-CfQNUy7g.js";import{S as p}from".
 
       </bim-panel-section>
     </bim-panel>
-    `);document.body.append(a);const C=l.create(()=>c`
+    `);document.body.append(a);const C=i.create(()=>c`
       <bim-button class="phone-menu-toggler" icon="solar:settings-bold"
         @click="${()=>{a.classList.contains("options-menu-visible")?a.classList.remove("options-menu-visible"):a.classList.add("options-menu-visible")}}">
       </bim-button>
