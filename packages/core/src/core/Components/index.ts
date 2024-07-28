ada621ea4ee3d880ccb04538c1c3e4e11df031e8
Antonio González Viegas <antoniogviegas@hotmail.com>
2024-07-28 22:43:07 +0200

    chore(core): bump core version

--- packages/core/src/core/Components/index.ts
+++ packages/core/src/core/Components/index.ts
@@ -14,7 +14,7 @@ export class Components implements Disposable {
   /**
    * The version of the @thatopen/components library.
    */
-  static readonly release = "2.1.14";
+  static readonly release = "2.1.15";
 
   /** {@link Disposable.onDisposed} */
   readonly onDisposed = new Event<void>();
