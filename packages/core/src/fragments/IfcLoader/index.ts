9e8cb41da8917a79a8ae559394203e397b96fb41
Antonio González Viegas <antoniogviegas@hotmail.com>
2024-08-05 13:21:01 +0200

    fix(core): use customLocateHandler in IfcLoader

--- packages/core/src/fragments/IfcLoader/index.ts
+++ packages/core/src/fragments/IfcLoader/index.ts
@@ -177,7 +177,7 @@ export class IfcLoader extends Component implements Disposable {
   async readIfcFile(data: Uint8Array) {
     const { path, absolute, logLevel } = this.settings.wasm;
     this.webIfc.SetWasmPath(path, absolute);
-    await this.webIfc.Init();
+    await this.webIfc.Init(this.settings.customLocateFileHandler || undefined);
     if (logLevel) {
       this.webIfc.SetLogLevel(logLevel);
     }
