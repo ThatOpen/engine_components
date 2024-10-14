e1a1c72cdb43afd0195bee18c0e5c5ebcb1bd672
Antonio González Viegas <antoniogviegas@hotmail.com>
2024-10-14 12:08:09 +0200

    fix(front): correct another outliner disposal bug

--- packages/front/src/fragments/Outliner/index.ts
+++ packages/front/src/fragments/Outliner/index.ts
@@ -17,7 +17,7 @@ export class Outliner extends OBC.Component implements OBC.Disposable {
 
   /** {@link OBC.Component.enabled} */
   get enabled() {
-    if (!this.world) {
+    if (!this.world || this.world.isDisposing) {
       return false;
     }
 
