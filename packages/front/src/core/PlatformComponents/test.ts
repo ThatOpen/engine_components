12b8e93e6475e34ce2d9a9c79a434fbf6d1a3df0
Antonio González Viegas <antoniogviegas@hotmail.com>
2024-10-24 13:49:25 +0200

    feat(front): test platform component

---
+++ packages/front/src/core/PlatformComponents/test.ts
@@ -0,0 +1,50 @@
+import * as OBC from "@thatopen/components";
+import Stats from "stats.js";
+import * as BUI from "@thatopen/ui";
+import * as OBCF from "../..";
+
+BUI.Manager.init();
+
+const container = document.getElementById("container")!;
+
+const components = new OBC.Components();
+
+const worlds = components.get(OBC.Worlds);
+
+const world = worlds.create<
+  OBC.SimpleScene,
+  OBC.SimpleCamera,
+  OBCF.RendererWith2D
+>();
+
+world.scene = new OBC.SimpleScene(components);
+world.renderer = new OBCF.RendererWith2D(components, container);
+world.camera = new OBC.SimpleCamera(components);
+
+components.init();
+
+world.camera.controls.setLookAt(5, 5, 5, 0, 0, 0);
+
+container.appendChild(world.renderer.three2D.domElement);
+
+const grids = components.get(OBC.Grids);
+grids.create(world);
+
+world.scene.three.background = null;
+
+const stats = new Stats();
+stats.showPanel(2);
+document.body.append(stats.dom);
+stats.dom.style.left = "0px";
+stats.dom.style.zIndex = "unset";
+world.renderer.onBeforeUpdate.add(() => stats.begin());
+world.renderer.onAfterUpdate.add(() => stats.end());
+
+(window as any).ThatOpenCompany = { OBC, BUI };
+
+const cloudComponents = components.get(OBCF.PlatformComponents);
+const fetched = await fetch("../../../../../resources/mock-cloud-component.js");
+const componentData = await fetched.text();
+const test = await cloudComponents.import(componentData);
+const ui = test.getUI();
+document.body.appendChild(ui[0].get());
--- packages/front/src/core/index.ts
+++ packages/front/src/core/index.ts
@@ -2,3 +2,4 @@ export * from "./Marker";
 export * from "./ClipEdges";
 export * from "./ShadowDropper";
 export * from "./PostproductionRenderer";
+export * from "./PlatformComponents";
