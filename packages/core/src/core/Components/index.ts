e1b18392d261d419dac6ed240ea6f50ddd8d5034
Juan Hoyos <juan.hoyos4@gmail.com>
2024-11-06 10:48:02 -0500

    chore: bump versions

--- packages/core/src/core/Components/index.ts
+++ packages/core/src/core/Components/index.ts
@@ -14,7 +14,7 @@ export class Components implements Disposable {
   /**
    * The version of the @thatopen/components library.
    */
-  static readonly release = "2.3.15";
+  static readonly release = "2.3.16";
 
   /** {@link Disposable.onDisposed} */
   readonly onDisposed = new Event<void>();
