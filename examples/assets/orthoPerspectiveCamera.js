29ebd0de11fc5fb53260b95a9e22ca77955e513a
GoulielmosFloros <82643379+GoulielmosFloros@users.noreply.github.com>
2025-02-16 13:15:27 +0000

    fix: mobile device orthographic camera zoom (#558)
    * fix: mobile device orthographic camera zoom
    
    Fix the orthographic camera's zoom functionality on mobile devices that it was not working.
    
    Added camera-controls package to enable extended actions to Orthographic camera. Added some functionalities to the camera when setting up the projection in the example.ts file.
    
    Reviewed-by: Goulielmos
    Refs: #531
    
    * fix: Fixed the issue in the libraries (projection.ts) instead of the example.ts file.

--- examples/assets/orthoPerspectiveCamera.js
+++ examples/assets/orthoPerspectiveCamera.js
@@ -1,4 +1,4 @@
-import{B as u,M as h,a as w}from"./web-ifc-api-r1ed24cU.js";import{S as C}from"./stats.min-GTpOrGrX.js";import{T as L,L as g,m as v}from"./index-ByMLC5eT.js";import{C as f,W as k,S as P,d as j,O,G as y}from"./index-B7zJT0sV.js";import"./_commonjsHelpers-Cpj98o6Y.js";const F=document.getElementById("container");let t=new f,c=t.get(k),e=c.create();e.scene=new P(t);e.renderer=new j(t,F);e.camera=new O(t);e.scene.setup();await e.camera.controls.setLookAt(3,3,3,0,0,0);t.init();e.scene.three.background=null;let l=new u,m=new h({color:"#6528D7"}),n=new w(l,m);n.position.set(0,.5,0);e.scene.three.add(n);e.meshes.add(n);let d=t.get(y),p=d.create(e);e.camera.projection.onChanged.add(()=>{const o=e.camera.projection.current;p.fade=o==="Perspective"});const r=new C;r.showPanel(2);document.body.append(r.dom);r.dom.style.left="0px";r.dom.style.zIndex="unset";e.renderer.onBeforeUpdate.add(()=>r.begin());e.renderer.onAfterUpdate.add(()=>r.end());L.init();const a=g.create(()=>v`
+import{B as u,M as h,a as w}from"./web-ifc-api-BlmMr04K.js";import{S as C}from"./stats.min-GTpOrGrX.js";import{T as L,L as g,m as f}from"./index-C8nqhRYO.js";import{C as v,W as k,O as P}from"./index-5w_8VFLQ.js";import{G as j}from"./index-Bn1byeNd.js";import{S as O,a as y}from"./simple-renderer-CvtLt1Hw.js";import"./_commonjsHelpers-Cpj98o6Y.js";const F=document.getElementById("container");let t=new v,c=t.get(k),e=c.create();e.scene=new O(t);e.renderer=new y(t,F);e.camera=new P(t);e.scene.setup();await e.camera.controls.setLookAt(3,3,3,0,0,0);t.init();e.scene.three.background=null;let l=new u,m=new h({color:"#6528D7"}),n=new w(l,m);n.position.set(0,.5,0);e.scene.three.add(n);e.meshes.add(n);let d=t.get(j),p=d.create(e);e.camera.projection.onChanged.add(()=>{const o=e.camera.projection.current;p.fade=o==="Perspective"});const r=new C;r.showPanel(2);document.body.append(r.dom);r.dom.style.left="0px";r.dom.style.zIndex="unset";e.renderer.onBeforeUpdate.add(()=>r.begin());e.renderer.onAfterUpdate.add(()=>r.end());L.init();const a=g.create(()=>f`
     <bim-panel active label="Orthoperspective Camera Tutorial" class="options-menu">
       <bim-panel-section collapsed label="Controls">
          
@@ -29,12 +29,12 @@ import{B as u,M as h,a as w}from"./web-ifc-api-r1ed24cU.js";import{S as C}from".
         
         <bim-button 
           label="Reset scene" 
-          @click="${async()=>{t.dispose(),t=new f,c=t.get(k),e=c.create(),e.scene=new P(t),e.renderer=new j(t,F),e.camera=new O(t),e.scene.setup(),await e.camera.controls.setLookAt(3,3,3,0,0,0),t.init(),e.scene.three.background=null,l=new u,m=new h({color:"#6528D7"}),n=new w(l,m),n.position.set(0,.5,0),e.scene.three.add(n),e.meshes.add(n),d=t.get(y),p=d.create(e),e.camera.projection.onChanged.add(()=>{const o=e.camera.projection.current;p.fade=o==="Perspective"})}}">  
+          @click="${async()=>{t.dispose(),t=new v,c=t.get(k),e=c.create(),e.scene=new O(t),e.renderer=new y(t,F),e.camera=new P(t),e.scene.setup(),await e.camera.controls.setLookAt(3,3,3,0,0,0),t.init(),e.scene.three.background=null,l=new u,m=new h({color:"#6528D7"}),n=new w(l,m),n.position.set(0,.5,0),e.scene.three.add(n),e.meshes.add(n),d=t.get(j),p=d.create(e),e.camera.projection.onChanged.add(()=>{const o=e.camera.projection.current;p.fade=o==="Perspective"})}}">  
         </bim-button>  
 
       </bim-panel-section>
     </bim-panel>
-    `);document.body.append(a);const S=g.create(()=>v`
+    `);document.body.append(a);const S=g.create(()=>f`
       <bim-button class="phone-menu-toggler" icon="solar:settings-bold"
         @click="${()=>{a.classList.contains("options-menu-visible")?a.classList.remove("options-menu-visible"):a.classList.add("options-menu-visible")}}">
       </bim-button>
