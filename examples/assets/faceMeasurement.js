65169624614af155c25b9b38a3eee634b67942b0
Goulielmos Floros <goulielmos13@hotmail.com>
2025-03-01 13:03:49 +0000

    fix: upgrade three to 0.170.0 #564
    Upgrade three to 0.170 at:
    packages/core
    packages/front
    
    When tried to update at 0.174.0, which is the latest an error occured for importing WebGLMultipleRenderTargets as this is missing from that updated version. Hence, 0.170.0 was selected. Conscious there may be a need for to be ugraded to the other repos as well.
    
    Reviewed-by: Goulielmos Floros
    Refs: #564

--- examples/assets/faceMeasurement.js
+++ examples/assets/faceMeasurement.js
@@ -1,4 +1,4 @@
-import{a as m}from"./web-ifc-api-r1ed24cU.js";import{S as b}from"./stats.min-GTpOrGrX.js";import{C as p,W as u,S as f,a as g,G as h,F as w}from"./index-4oEgnBmA.js";import{T as y,L as i,m as c}from"./index-ByMLC5eT.js";import{W as k,P as S}from"./index-CDKMALq_.js";import"./_commonjsHelpers-Cpj98o6Y.js";const r=document.getElementById("container"),s=new p,L=s.get(u),e=L.create();e.scene=new f(s);e.renderer=new k(s,r);e.camera=new g(s);s.init();e.camera.controls.setLookAt(5,5,5,0,0,0);e.scene.setup();const v=s.get(h);v.create(e);e.scene.three.background=null;const P=new w(s),A=await fetch("https://thatopen.github.io/engine_components/resources/small.frag"),C=await A.arrayBuffer(),D=new Uint8Array(C),d=P.load(D);e.scene.three.add(d);for(const t of d.children)t instanceof m&&e.meshes.add(t);const n=s.get(S);n.world=e;n.enabled=!0;r.ondblclick=()=>n.create();let l;window.addEventListener("keydown",t=>{t.code==="KeyO"?n.delete():t.code==="KeyS"?(l=n.get(),n.deleteAll()):t.code==="KeyL"&&l&&n.set(l)});const o=new b;o.showPanel(2);document.body.append(o.dom);o.dom.style.left="0px";o.dom.style.zIndex="unset";e.renderer.onBeforeUpdate.add(()=>o.begin());e.renderer.onAfterUpdate.add(()=>o.end());y.init();const a=i.create(()=>c`
+import{a as m}from"./web-ifc-api-D8KQoGas.js";import{S as b}from"./stats.min-GTpOrGrX.js";import{C as p,W as u,S as f,a as g,G as h,F as w}from"./index-B_l14CHw.js";import{T as y,L as i,m as c}from"./index-ByMLC5eT.js";import{X as k,N as S}from"./index-BCbMsEjB.js";import"./_commonjsHelpers-Cpj98o6Y.js";const r=document.getElementById("container"),s=new p,L=s.get(u),e=L.create();e.scene=new f(s);e.renderer=new k(s,r);e.camera=new g(s);s.init();e.camera.controls.setLookAt(5,5,5,0,0,0);e.scene.setup();const v=s.get(h);v.create(e);e.scene.three.background=null;const A=new w(s),C=await fetch("https://thatopen.github.io/engine_components/resources/small.frag"),D=await C.arrayBuffer(),x=new Uint8Array(D),d=A.load(x);e.scene.three.add(d);for(const t of d.children)t instanceof m&&e.meshes.add(t);const n=s.get(S);n.world=e;n.enabled=!0;r.ondblclick=()=>n.create();let l;window.addEventListener("keydown",t=>{t.code==="KeyO"?n.delete():t.code==="KeyS"?(l=n.get(),n.deleteAll()):t.code==="KeyL"&&l&&n.set(l)});const o=new b;o.showPanel(2);document.body.append(o.dom);o.dom.style.left="0px";o.dom.style.zIndex="unset";e.renderer.onBeforeUpdate.add(()=>o.begin());e.renderer.onAfterUpdate.add(()=>o.end());y.init();const a=i.create(()=>c`
   <bim-panel active label="Face Measurement Tutorial" class="options-menu">
       <bim-panel-section collapsed label="Controls">
           <bim-label>Create dimension: Double click</bim-label>  
@@ -18,8 +18,8 @@ import{a as m}from"./web-ifc-api-r1ed24cU.js";import{S as b}from"./stats.min-GTp
 
       </bim-panel-section>
     </bim-panel>
-    `);document.body.append(a);const x=i.create(()=>c`
+    `);document.body.append(a);const P=i.create(()=>c`
       <bim-button class="phone-menu-toggler" icon="solar:settings-bold"
         @click="${()=>{a.classList.contains("options-menu-visible")?a.classList.remove("options-menu-visible"):a.classList.add("options-menu-visible")}}">
       </bim-button>
-    `);document.body.append(x);
+    `);document.body.append(P);
