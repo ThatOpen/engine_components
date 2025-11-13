35745b46377b0b8d1fcfb3830f366fe1b1d97fbe
Antonio González Viegas <antoniogviegas@hotmail.com>
2025-11-13 20:21:20 +0100

    chore: clean up old ids parameter

--- packages/core/src/openbim/IDSSpecifications/src/importers/property.ts
+++ packages/core/src/openbim/IDSSpecifications/src/importers/property.ts
@@ -2,21 +2,17 @@ import { Components } from "../../../../core/Components";
 import { IDSFacet, IDSProperty } from "../facets";
 import { getParameterValue } from "./parameter";
 
-export const createPropertyFacets = (
-  components: Components,
-  elements: any,
-  parseNumericString: boolean,
-) => {
+export const createPropertyFacets = (components: Components, elements: any) => {
   const facets: IDSFacet[] = [];
   for (const element of elements) {
     const psetParameter = element.propertySet;
     const baseNameParameter = element.baseName;
-    const pset = getParameterValue(psetParameter, parseNumericString);
-    const baseName = getParameterValue(baseNameParameter, parseNumericString);
+    const pset = getParameterValue(psetParameter);
+    const baseName = getParameterValue(baseNameParameter);
     if (!(baseName && pset)) continue;
     const facet = new IDSProperty(components, pset, baseName);
     if (element.cardinality) facet.cardinality = element.cardinality;
-    const value = getParameterValue(element.value, parseNumericString);
+    const value = getParameterValue(element.value);
     facet.value = value;
     facet.dataType = element.dataType;
     facet.uri = element.uri;
