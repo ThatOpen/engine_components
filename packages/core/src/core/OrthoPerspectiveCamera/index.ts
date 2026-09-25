a16010c4e161ae7cb6b687f89269a91e608730b0
Shachar <34343793+ShaMan123@users.noreply.github.com>
2026-09-25 15:45:31 +0300

    fix(OrthoPerspectiveCamera): safeguard zero canvas size
    * fix(OrthoPerspectiveCamera): safeguard zero size
    
    * fixes

--- packages/core/src/core/OrthoPerspectiveCamera/index.ts
+++ packages/core/src/core/OrthoPerspectiveCamera/index.ts
@@ -223,6 +223,11 @@ export class OrthoPerspectiveCamera extends SimpleCamera {
 
     const size = this.currentWorld.renderer.getSize();
 
+    if (this.previousSize.x === 0 || this.previousSize.y === 0) {
+      this.previousSize.copy(size);
+      return;
+    }
+
     const previousHeight = this.threeOrtho.top;
     const previousWidth = this.threeOrtho.right;
 
